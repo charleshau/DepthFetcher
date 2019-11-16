c1260f770df27d223cd02516e97cd72dd4932d39
Charles He <mhe86@uwo.ca>
2019-11-16 15:52:02 -0500

    Initial Commit

---
+++ DepthFetcher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DepthFetcher
+//
+//  Created by Charles on 2019-11-16.
+//  Copyright © 2019 Charles He. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
