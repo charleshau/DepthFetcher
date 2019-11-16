9fd8c17a3b16fc6a877546509dc3549b23cdb9b8
Charles He <mhe86@uwo.ca>
2019-11-16 16:43:44 -0500

    Init

--- DepthFetcher/main.swift
+++ DepthFetcher/main.swift
@@ -2,11 +2,20 @@
 //  main.swift
 //  DepthFetcher
 //
-//  Created by Charles on 2019-11-16.
+//  Created by Charles He on 2019-11-16.
 //  Copyright © 2019 Charles He. All rights reserved.
 //
 
 import Foundation
+import AppKit
+import AVFoundation
 
-print("Hello, World!")
 
+let fileName = "/Users/Charles/Library/Mobile Documents/com~apple~CloudDocs/Project/DepthFetcher/DepthFetcher/test00.jpg"
+
+if let image = NSImage(byReferencingFile: fileName)
+{
+    print("image size \(image.size.width):\(image.size.height)")
+}
+
+depthDataMapImage.save(as: "profile", fileType: .png)
